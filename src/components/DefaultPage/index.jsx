b66eac84b818261d8df14b2b20e04062991bcda8
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-26 20:44:49 -0300

    feat: style component added

--- src/components/DefaultPage/index.jsx
+++ src/components/DefaultPage/index.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const DefaultPage = () => {
     return (
-        <main>
+        <main className="default-page">
             <Outlet />
         </main>
     );
