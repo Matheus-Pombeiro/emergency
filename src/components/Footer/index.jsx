8f9319c9ef9cdb1e7a78706e58ac9c61760b09e7
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-27 17:08:15 -0300

    feat: Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    const date = new Date;
+    const year = date.getFullYear();
+
+    return (
+        <footer className="footer">
+            <p className="footer-p"><a className="footer-a" href="https://www.linkedin.com/in/matheus-pombeiro/" target="_blank" rel="noopener noreferrer">Matheus Pombeiro</a> | {year}</p>
+        </footer>
+    );
+};
+
+export default Footer;
