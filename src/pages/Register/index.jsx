0bc28def9562e68a6d015ca17ea69d1a0b90e2d3
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-28 13:39:17 -0300

    feat: 'InputText', 'DropDown' and 'InputRadio' references

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,22 @@
 import { useTranslation } from "react-i18next";
+
+import { useState } from "react";
+
 import Title from "../../components/Title";
+import InputText from "../../components/InputText";
+import DropDown from "../../components/DropDown";
+import InputRadio from "../../components/InputRadio";
 
 const Register = () => {
     const { t } = useTranslation();     // Translation reference
+
+    // Declare an array to keep the doctor options
+    const [doctorData, setDoctorData] = useState([
+        "Cardiologist", "General Practitioner", "Ophthalmologist", "Orthopedist", "Endocrinologist", "Gastroenterologist"
+    ]);
+
+    // Declare a const to keep the input radio name
+    const inputRadioName = "Urgency Level";
 
     return (
         <>
@@ -10,6 +24,72 @@ const Register = () => {
                 title="Emergency"
                 subtitle="Please, register a patient next"
             />
+
+            <form className="form">
+                <InputText 
+                    content="Name"
+                    type="text"
+                    required={true}
+                />
+
+                <InputText 
+                    content="Age"
+                    type="number"
+                    required={true}
+                />
+
+                <InputText 
+                    content="Photo"
+                    type="text"
+                    required={false}
+                />
+
+                <InputText 
+                    content="City of Birth"
+                    type="text"
+                    required={true}
+                />
+
+                <DropDown 
+                    content="Doctor"
+                    data={doctorData}
+                    required={true}
+                />
+
+                <fieldset className="fieldset">
+                    <label
+                        className="label"
+                        htmlFor="Imediate"
+                    >
+                        {inputRadioName}
+                    </label>
+
+                    <InputRadio 
+                        content="Imediate"
+                        id="Imediate"
+                        name={inputRadioName}
+                    />
+
+                    <InputRadio 
+                        content="Needs Care"
+                        id="Needs Care"
+                        name={inputRadioName}
+                    />
+
+                    <InputRadio 
+                        content="Can Wait"
+                        id="Can Wait"
+                        name={inputRadioName}
+                    />
+                </fieldset>
+
+                <button 
+                    type="submit"
+                    className="submit-btn"
+                >
+                    Register
+                </button>
+            </form>
         </>
     );
 };
