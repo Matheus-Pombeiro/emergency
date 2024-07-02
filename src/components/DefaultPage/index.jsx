f11af10d30ed4aeaba864d4eac93e29415364546
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-07-02 13:38:17 -0300

    refactor: 'mediaQuerry' style components

--- src/components/DefaultPage/index.jsx
+++ src/components/DefaultPage/index.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const DefaultPage = () => {
     return (
-        <main className="default-page">
+        <main className="default-page default-page-mobile xl:default-page-desktop">
             <Outlet />
         </main>
     );
