3351e8d7f41afa8c2a659be4ebee2da594669f08
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-07-02 13:11:59 -0300

    feat: 'Thead' component with its structure and stylization

---
+++ src/components/Table/Thead/index.jsx
@@ -0,0 +1,25 @@
+const Thead = () => {
+    return (
+        <thead>
+            <tr className="table-row">
+                <th className="table-cell">
+                    Urgency
+                </th>
+                <th className="table-cell">
+                    Name
+                </th>
+                <th className="table-cell">
+                    Doctor
+                </th>
+                <th className="table-cell">
+                    Status
+                </th>
+                <th className="table-cell">
+                    Delete
+                </th>
+            </tr>
+        </thead>
+    );
+};
+
+export default Thead;
