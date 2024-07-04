749f66d3777136fae698c91b3f67553e3cf60ba2
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-07-03 22:06:50 -0300

    feat: 'search' and 'filter' prop dhriling

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -2,12 +2,16 @@ import Tbody from "./Tbody";
 import Tfooter from "./Tfooter";
 import Thead from "./Thead";
 
-const Table = ({ patients }) => {
+const Table = ({ patients, search, filter }) => {
     return (
         <div className="table-container">
             <table className="table">
                 <Thead />
-                <Tbody patients={patients}/>
+                <Tbody 
+                    patients={patients}
+                    search={search}
+                    filter={filter}
+                />
                 <Tfooter patients={patients}/>
             </table>
         </div>
