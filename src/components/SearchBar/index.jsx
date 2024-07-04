c091e4440b845087a02493ab287eb4b2b7d1bb8d
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-07-03 22:06:06 -0300

    feat: received the 'search' input value and keep it into the 'search' state

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,12 +1,14 @@
 import { IoSearchOutline } from "react-icons/io5";
 
-const SearchBar = () => {
+const SearchBar = ({ search, setSearch }) => {
     return (
         <fieldset className="search-bar-container">
             <input
                 className="search-bar-input"
                 type="text"
                 placeholder="Enter the patient's name..."
+                value={search}
+                onChange={e => setSearch(e.target.value)}
             />
             <IoSearchOutline size={25}/>
         </fieldset>
