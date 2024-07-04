1f037157adc348b3566d48503aae6d901b275e8c
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-07-03 22:08:39 -0300

    feat: 'search' and 'filter' prop dhriling

--- src/pages/WaitingList/index.jsx
+++ src/pages/WaitingList/index.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import DropDown from "../../components/DropDown";
 import SearchBar from "../../components/SearchBar";
 import Table from "../../components/Table";
 import Title from "../../components/Title";
+import Filter from "../../components/Filter";
 
-const WaitingList = ({ patients }) => {
+const WaitingList = ({ patients, search, setSearch, filter, setFilter }) => {
+    // Declare an array to keep the filter options
+    const [filterOptions, setFilterOptions] = useState([
+        "Urgency", "Name", "Doctor"  
+    ]);
+
     return (
         <>
             <Title 
@@ -10,10 +18,24 @@ const WaitingList = ({ patients }) => {
                 subtitle="Next you can see the waiting list"
             />
 
-            <SearchBar />
+            <SearchBar 
+                search={search}
+                setSearch={setSearch}
+            />
+
+            <Filter 
+                content="Order"
+                data={filterOptions}
+                filter={filter}
+                setFilter={setFilter}
+            />
 
             {patients.length > 0 && (
-                <Table patients={patients}/>
+                <Table 
+                    patients={patients}
+                    search={search}
+                    filter={filter}
+                />
             )}
         </>
     );
