b45e954f5cbbec826526a212ac005e3627801a66
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-07-07 11:58:55 -0300

    feat: added an object to set the buttons' size

--- src/components/ThemeSwitcher/index.jsx
+++ src/components/ThemeSwitcher/index.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 import { IoSunny, IoMoonSharp  } from "react-icons/io5";
 
-const ThemeSwitcher = ({ btnSet }) => {
+const ThemeSwitcher = () => {
     const [themeBtn, setThemeBtn] = useState(true); // Helps the theme icons
 
     // Changes the app's theme
     const handleTheme = () => {
         document.documentElement.classList.toggle("dark");
         setThemeBtn(!themeBtn);
+    };
+
+    const btnSet = {    // Set a default size value for the buttons
+        size: 35
     };
 
     return (
